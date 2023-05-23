c8517ec20267125cff7e88d5666222936eb68b79
yongjunhan <hanyongjun.dev@gmail.com>
2023-05-23 16:59:53 +0900

    Provider Observer (Logging)

--- riverpods/lib/main.dart
+++ riverpods/lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpods/riverpod/provider_observer.dart';
 import 'package:riverpods/screens/home.dart';
 
 void main() {
   runApp(
     //provider를 사용하려면 최상위 materialapp 을 감싸주어야 한다
-    const ProviderScope(
-      child: MaterialApp(
+    ProviderScope(
+      observers: [
+        Logger(),
+      ],
+      child: const MaterialApp(
         home: HomeScreen(),
       ),
     ),
